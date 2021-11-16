5cbcec47572d432111314b7e49e6f15a9c266235
Ilhaam Ghiffari <85248188+Haamergo@users.noreply.github.com>
2021-11-16 12:00:21 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,17 @@
+def checkprime():
+    print("===========Cek Bilangan Prima=============")
+    angka = int(input("Masukkan Angka:"))
+    if angka > 1:
+        for i in range(2, int(angka/2)+1):
+            if (angka % i) == 0:
+                print("{num} bukan angka prima".format(num = angka))
+                break
+        else:
+            print("{num} adalah angka prima".format(num = angka))
+    else :
+        print("{num} bukan angka prima".format(num = angka))
+
+while True:
+    checkprime()
+    if input("Repeat the program? (Y/N)").strip().upper() != 'Y':
+        break
